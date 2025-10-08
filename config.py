83147b0837f5cd26326dcb8053fbea906342c27f
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-08 21:26:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,15 +36,16 @@ SUDO_USERS.append(OWNER_ID)
 
 # ------------- CLIENTS -------------
 
-X1 = TelegramClient('X1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-X2 = TelegramClient('X2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-X3 = TelegramClient('X3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-X4 = TelegramClient('X4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-X5 = TelegramClient('X5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-X6 = TelegramClient('X6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-X7 = TelegramClient('X7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-X8 = TelegramClient('X8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-X9 = TelegramClient('X9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-X10 = TelegramClient('X10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+X1 = TelegramClient('X1', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN)
+X2 = TelegramClient('X2', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN2)
+X3 = TelegramClient('X3', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN3)
+X4 = TelegramClient('X4', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN4)
+X5 = TelegramClient('X5', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN5)
+X6 = TelegramClient('X6', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN6)
+X7 = TelegramClient('X7', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN7)
+X8 = TelegramClient('X8', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN8)
+X9 = TelegramClient('X9', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN9)
+X10 = TelegramClient('X10', 21134445, 231c18ea7273824491d6bf05425ab74e).start(bot_token=BOT_TOKEN10)
+
 
 
